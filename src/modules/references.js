e9530f099ba4aca2949676b0180b20fe7beb59cd
git config --global user.name inrayzgit config --global user.name <inrayz@bk.ru>
2021-03-25 01:37:13 +0600

    fix

--- src/modules/references.js
+++ src/modules/references.js
@@ -2,16 +2,20 @@
 
 const references = () => {
     const anchors = document.querySelectorAll('a[href*="#"]');
-    console.log(anchors);
 
     for (let anchor of anchors) {
       anchor.addEventListener('click', function (event){
         event.preventDefault();
         const blockId = anchor.getAttribute('href');
-        document.querySelector('' + blockId).scrollIntoView({
+        if (blockId === '#'){
+          return;
+        }else{
+          document.querySelector('' + blockId).scrollIntoView({
           behavior: 'smooth',
           block: 'start'
         });
+        }
+        
       });
     }
   };
